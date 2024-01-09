316a3d85a523a26eae95c0ba8f3f9289128cecbb
Islam <50731444+xislam@users.noreply.github.com>
2024-01-09 22:13:18 +0600

    'corsheaders.middleware.CorsMiddleware'

--- root/settings.py
+++ root/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-0pjng_b6^h+mfg)2d_==aaa@xyp(*4h6*s%k_v7dr7(1c@33pj
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*', '195.49.210.86']
 
 # Application definition
 
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'rest_framework',
     'drf_yasg',
     # apps
@@ -53,6 +54,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'root.urls'
@@ -129,3 +131,20 @@ MEDIA_URL = 'media/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOW_ALL_ORIGINS = True  # Установите в True, чтобы разрешить все источники (не рекомендуется в продакшене)
+CORS_ALLOWED_ORIGINS = [
+    "http://195.49.210.86"
+
+]
+CORS_ALLOW_METHODS = [
+    "GET",
+    "POST",
+    "PUT",
+    "PATCH",
+    "DELETE",
+]
+CORS_ALLOW_HEADERS = [
+    "Authorization",
+    "Content-Type",
+]
