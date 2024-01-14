0327b801db9a64399e6acc464333fbadd36123dd
Islam <50731444+xislam@users.noreply.github.com>
2024-01-14 17:59:07 +0600

    profile

--- basket/urls.py
+++ basket/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from basket.api import OrderListView
+from basket.api import OrderListView, OrderCreateView
 
 urlpatterns = [
-    path('orders/', OrderListView.as_view(), name='order-create'),
+    path('orders/', OrderListView.as_view(), name='order-list'),
+    path('orders/', OrderCreateView.as_view(), name='order-create'),
     # Add other URL patterns as needed
 ]
