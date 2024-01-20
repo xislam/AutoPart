f096c96ee4bc8f6f34d4bb5b3bb7ba92979a820b
nurkadyr <nurkadyr@digital-ms.com>
2024-01-20 22:15:36 +0600

    run_parser

--- products/models.py
+++ products/models.py
@@ -71,6 +71,7 @@ class Product(models.Model):
     external_color = models.CharField(verbose_name='Внешний цвет', max_length=100, blank=True, null=True)
     is_popular = models.BooleanField(default=False, verbose_name='Популярное')
     old_price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Стара цена')
+    price_in_won = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Цена на вон')
     new_price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Новая цена', null=True, blank=True)
 
     def __str__(self):
