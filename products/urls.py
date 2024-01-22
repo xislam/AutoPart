e282109e9ed287c567297c9aae672438dcc0f33d
Islam <50731444+xislam@users.noreply.github.com>
2024-01-22 20:57:54 +0600

    category

--- products/urls.py
+++ products/urls.py
@@ -2,15 +2,18 @@ from django.urls import path
 
 from products.api import ProductListView, CarMakeListView, CarNameListView, PopularProductListView, CategoryListView, \
     ProductDetailView, ProductSearchView, CategoryListView2
+from products.views import ProductsHTMLView
 
 urlpatterns = [
-    path('products/', ProductListView.as_view(), name='product-list'),
+    path('api/products_html/', ProductListView.as_view(), name='product-list'),
     path('products_s/', ProductSearchView.as_view(), name='product-list'),
     path('products_category/', CategoryListView2.as_view(), name='product-list'),
     path('carmakes/', CarMakeListView.as_view(), name='carmake-list'),
     path('carnames/', CarNameListView.as_view(), name='carname-list'),
-    path('popular_products/', PopularProductListView.as_view(), name='popular-product-list'),
+    path('popular_api/products_html/', PopularProductListView.as_view(), name='popular-product-list'),
     path('categories/', CategoryListView.as_view(), name='category-list'),
-    path('products/<int:id>/', ProductDetailView.as_view(), name='product-detail'),
+    path('api/products_html/<int:id>/', ProductDetailView.as_view(), name='product-detail'),
+
+    path('products_html/', ProductsHTMLView.as_view(), name='products'),
 
 ]
