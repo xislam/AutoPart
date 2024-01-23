ff7199b3425823374cffd90388eb73e83e8bb77c
Islam <50731444+xislam@users.noreply.github.com>
2024-01-23 23:43:49 +0600

    category

--- accounts/static/scripts/carts.js
+++ accounts/static/scripts/carts.js
@@ -90,3 +90,22 @@ document.addEventListener('DOMContentLoaded', function () {
         window.location.href = "/api/profile_html"
     })
 });
+
+function getSecureCookie(name) {
+    var nameEQ = name + '=';
+    var cookies = document.cookie.split(';');
+
+    for (var i = 0; i < cookies.length; i++) {
+        var cookie = cookies[i];
+
+        while (cookie.charAt(0) === ' ') {
+            cookie = cookie.substring(1, cookie.length);
+        }
+
+        if (cookie.indexOf(nameEQ) === 0) {
+            return decodeURIComponent(cookie.substring(nameEQ.length, cookie.length));
+        }
+    }
+
+    return null;
+}
